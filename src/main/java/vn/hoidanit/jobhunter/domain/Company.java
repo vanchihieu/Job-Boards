f74994032dc92752b854bbca9eb190eedd1d5a43
Van Chi Hieu <vanchihieu3@gmail.com>
2024-06-27 07:41:18 +0700

    Model Company

---
+++ src/main/java/vn/hoidanit/jobhunter/domain/Company.java
@@ -0,0 +1,34 @@
+package vn.hoidanit.jobhunter.domain;
+
+import java.time.Instant;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Table(name = "companies")
+@Entity
+@Getter
+@Setter
+public class Company {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    private String name;
+
+    @Column(columnDefinition = "MEDIUMTEXT")
+    private String description;
+
+    private String address;
+
+    private String logo;
+
+    private Instant createdAt;
+
+    private Instant updatedAt;
+
+    private String createdBy;
+
+    private String updatedBy;
+}
