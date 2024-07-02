2c63bd5e99c8c409e3fce417c815a1f01a304ce5
Van Chi Hieu <vanchihieu3@gmail.com>
2024-07-02 12:39:26 +0700

    Cron Job

--- src/main/java/vn/hoidanit/jobhunter/controller/EmailController.java
+++ src/main/java/vn/hoidanit/jobhunter/controller/EmailController.java
@@ -22,6 +22,8 @@ public class EmailController {
 
     @GetMapping("/email")
     @ApiMessage("Send simple email")
+    // @Scheduled(cron = "*/30 * * * * *")
+    // @Transactional
     public String sendSimpleEmail() {
         this.subscriberService.sendSubscribersEmailJobs();
         return "ok";
